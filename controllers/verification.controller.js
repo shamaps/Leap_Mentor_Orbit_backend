687855c4e16e5e5fa1751d0f057806ab4c71d333
Vishal Kamath <vishal.kamath@nineleaps.com>
2026-03-26 00:55:41 +0530

    Change SMTP port and security settings

--- controllers/verification.controller.js
+++ controllers/verification.controller.js
@@ -7,8 +7,8 @@ const VerificationToken = require("../models/VerificationToken");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT || 465),
-  secure: true,
+  port: Number(process.env.SMTP_PORT || 587),
+  secure: false,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
 });
 
